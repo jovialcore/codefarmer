073e169f310e34c7e905137b744ca9cc7304c8b8
devchidi <jollydosky@gmail.com>
2018-04-15 17:52:53 +0100

    my first git bash work on my repo ! wow!

---
+++ hotelsng.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>jovish</title>
+	 <meta charset="utf-8"> 
+	 <link rel="shortcut icon" type="image/png" 
+	href="mygoodest.png" >
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style>
+* {
+    box-sizing: border-box;
+}
+
+body {
+    margin: 0 auto;
+    font-family: Arial;
+	max-width:950px;
+   background-color:darkgrey;	
+}
+
+.header{
+text-align:center; 
+background-color:white;
+font-family:tahoma;
+padding:15px;
+font-size: 41.1237px;
+color: #BF6262;
+border:0px solid grey;
+margin-bottom:12px
+}
+
+.mainBody
+{
+border:1px solid grey;
+ height:500px;
+position:relative;
+margin-bottom:12px
+}
+.mainBody > img {
+width:100%;
+height:100%;
+}
+
+.text {
+position:absolute;
+font-size: 41.1237px;
+
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+left:12px;
+top: 79px;
+color: #F70505;
+}
+
+.footer {
+text-align:center;
+border:0px solid grey;
+padding:15px;
+background-color:white;
+font-family:tahoma;
+color:#BF6262;
+font-size: 29.6399px;
+font-weight: 600px;
+}
+</style>
+</head>
+
+<body>
+<div class="header">  JOVIAL;Farmer By Day, Programmer By Night</div>
+
+<div class="mainBody"> 
+<img src="enjoy.jpg">
+<div class="text">  HI! Chidiebere is a farmer that codes. A full 
+stack web developer
+
+From Nigeria <br/> <br/>
+ 	<?php
+date_default_timezone_set("Africa/Lagos");   //India time (GMT+5:30)
+echo date('d-m-Y H:i:s');
+?>
+
+</div>
+ </div>
+
+<footer class="footer"> footer</footer>
+
+</body>
+</html>
